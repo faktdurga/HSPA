76a9c5840b35cfbad1895331841533e408653de5
faktdurga <mohited24@gmail.com>
2024-06-23 10:02:00 +0530

    startcode

---
+++ src/app/property/property-card/property-card.component.ts
@@ -0,0 +1,19 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector:'app-property-card',
+
+  templateUrl:'property-card.component.html',
+  styleUrl:'property-card.component.css'
+
+}
+)
+export class PropertyCardComponent
+{
+  Property : any={
+    "Id" : 1,
+    "Type": "House",
+    "Price" : 12000
+  };
+
+}
